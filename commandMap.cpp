ecb58c87e8fa75c49030a6fbd4b0ac823327f963
RosenbergYehuda <yehudazevr@gmail.com>
2022-07-19 10:23:07 +0300

    remove ::std

--- commandMap.cpp
+++ commandMap.cpp
@@ -4,13 +4,13 @@
 CommandMap::CommandMap()
 {
     // Inserting values by using [] operator
-    umap["openDataServer"] = std::make_unique <ServerCommand>();
-    umap["connect"] = std::make_unique<ClientCommand>();
-    umap["var"] = std::make_unique<VarCommand>();
-    umap["set"] = std::make_unique<SetCommand>();
-    umap["while"] = std::make_unique<WhileCommand>();
-    umap["print"] = std::make_unique<PrintCommand>();
-    umap["sleep"] = std::make_unique<SleepCommand>();
+    umap["openDataServer"] = make_unique <ServerCommand>();
+    umap["connect"] = make_unique<ClientCommand>();
+    umap["var"] = make_unique<VarCommand>();
+    umap["set"] = make_unique<SetCommand>();
+    umap["while"] = make_unique<WhileCommand>();
+    umap["print"] = make_unique<PrintCommand>();
+    umap["sleep"] = make_unique<SleepCommand>();
 };
 
 unordered_map<string, unique_ptr <Command>> CommandMap::umap;
